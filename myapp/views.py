0a26973e0f3d06bfb1250759fd76872cab1ee742
AhsanDev7 <ahsan.ghafoor@techleadz.com>
2024-12-23 16:29:38 +0500

    Feature: 3rd party API integration and it's testing using mock

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+import requests
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .filters import BookFilter
 from rest_framework.filters import SearchFilter
@@ -6,9 +7,8 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.parsers import MultiPartParser, FormParser
-from .models import Book, Order, Genre, Author, Publisher, Review
-from .serializers import BookSerializer, OrderSerializer,BookSummarySerializer, GenreSerializer, AuthorSerializer, PublisherSerializer, ReviewSerializer
-
+from myapp.models import Book, Order, Genre, Author, Publisher, Review
+from myapp.serializers import BookSerializer, OrderSerializer,BookSummarySerializer, GenreSerializer, AuthorSerializer, PublisherSerializer, ReviewSerializer
 
 class CustomPagination(PageNumberPagination):
     page_size = 5
@@ -29,6 +29,32 @@ class BookViewSet(viewsets.ModelViewSet):
         books = Book.objects.all()
         serializer = BookSummarySerializer(books, many=True)
         return Response(serializer.data)
+ 
+    # Custom action to search books using the Open Library API
+    @action(detail=False, methods=['get'], url_path='open-library')
+    def open_library(self, request):
+        query = request.query_params.get("query", "")
+        if not query:
+            return Response(
+                {"error": "Query parameter is required."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        api_url = "https://openlibrary.org/search.json"
+        params = {
+            "q": query,
+            "fields": "key,title,author_name,editions,description",
+        }
+        try:
+            response = requests.get(api_url, params=params)
+            response.raise_for_status()
+            data = response.json()
+            return Response(data, status=status.HTTP_200_OK)
+        except requests.exceptions.RequestException as e:
+            return Response(
+                {"error": str(e)}, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
